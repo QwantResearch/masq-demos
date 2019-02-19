cc33ded644cd81b0a22ed4b3079c83660cb5ec7f
G-Ray <g.bonneville@qwant.com>
2019-02-19 15:17:20 +0100

    Avoid blocked masq popup by just calling window.open once

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -67,16 +67,7 @@ class App extends Component {
     const { link } = this.state
     if (!link) return
 
-    if (this.masqPopupWindow) {
-      // close previous popup if any
-      this.masqPopupWindow.close()
-
-      this.masqPopupWindow = window.open(link, 'masq', 'height=700,width=500')
-    } else {
-      this.masqPopupWindow = window.open('', 'masq', 'height=700,width=500')
-      this.masqPopupWindow.close()
-      this.masqPopupWindow = window.open(link, 'masq', 'height=700,width=500')
-    }
+    this.masqPopupWindow = window.open(link, 'masq', 'height=700,width=500')
 
     try {
       await this.masq.logIntoMasq(this.state.stayConnected)
