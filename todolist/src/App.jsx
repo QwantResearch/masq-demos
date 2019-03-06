f9893c34fc1f9563c0affa1be26e338c1f53dd51
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-03-06 16:07:18 +0100

    App.jsx: use an env var to enable stun turn server settings

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -19,25 +19,27 @@ const MASQ_APP_BASE_URL = process.env.REACT_APP_MASQ_APP_BASE_URL
 
 
 let STUN_TURN = []
-if (process.env.REACT_APP_STUN_URLS) {
-  const urls = process.env.REACT_APP_STUN_URLS.split(',').map(
-    u => {
-      return { urls: u }
-    })
-  STUN_TURN = STUN_TURN.concat(urls)
-}
+if (process.env.REACT_APP_REMOTE_WEBRTC === 'true') {
+  if (process.env.REACT_APP_STUN_URLS) {
+    const urls = process.env.REACT_APP_STUN_URLS.split(',').map(
+      u => {
+        return { urls: u }
+      })
+    STUN_TURN = STUN_TURN.concat(urls)
+  }
 
-if (process.env.REACT_APP_TURN_URLS) {
-  const urls = process.env.REACT_APP_TURN_URLS.split(',').map(
-    u => {
-      const splitted = u.split('|')
-      return {
-        urls: splitted[0],
-        username: splitted[1],
-        credential: splitted[2]
-      }
-    })
-  STUN_TURN = STUN_TURN.concat(urls)
+  if (process.env.REACT_APP_TURN_URLS) {
+    const urls = process.env.REACT_APP_TURN_URLS.split(',').map(
+      u => {
+        const splitted = u.split('|')
+        return {
+          urls: splitted[0],
+          username: splitted[1],
+          credential: splitted[2]
+        }
+      })
+    STUN_TURN = STUN_TURN.concat(urls)
+  }
 }
 
 const APP = {
